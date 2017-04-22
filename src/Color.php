e7b054848d1bbf7262c343568676fc51cd775471
wujunze <itwujunze@163.com>
2017-04-22 18:23:27 +0800

    optimizing echo

--- src/Color.php
+++ src/Color.php
@@ -13,6 +13,35 @@ class Colors {
     private $foreground_colors = array();
     private $background_colors = array();
 
+    private static $foregroundColors = [
+        'black' => '0;30',
+        'dark_gray' => '1;30',
+        'blue' => '0;34',
+        'light_blue' => '1;34',
+        'green' => '0;32',
+        'light_green' => '1;32',
+        'cyan' => '0;36',
+        'light_cyan' => '1;36',
+        'red' => '0;31',
+        'light_red' => '1;31',
+        'purple' => '0;35',
+        'light_purple' => '1;35',
+        'brown' => '0;33',
+        'yellow' => '1;33',
+        'light_gray' => '0;37',
+        'white' => '1;37',
+    ];
+    private static $backgroundColors = [
+        'black' => '40',
+        'red' => '41',
+        'green' => '42',
+        'yellow' => '43',
+        'blue' => '44',
+        'magenta' => '45',
+        'cyan' => '46',
+        'light_gray' => '47',
+    ];
+
     public function __construct() {
         // Set up shell colors
         $this->foreground_colors['black'] = '0;30';
@@ -69,6 +98,70 @@ class Colors {
     // Returns all background color names
     public function getBackgroundColors() {
         return array_keys($this->background_colors);
+    }
+
+
+
+    /**
+     * 获取带颜色的文字
+     * @param string $string black|dark_gray|blue|light_blue|green|light_green|cyan|light_cyan|red|light_red|purple|brown|yellow|light_gray|white
+     * @param string|null $foregroundColor 前景颜色 black|red|green|yellow|blue|magenta|cyan|light_gray
+     * @param string|null $backgroundColor 背景颜色 同$foregroundColor
+     * @return string
+     */
+    public static function initColoredString(
+        $string,
+        $foregroundColor = null,
+        $backgroundColor = null
+    ) {
+        $coloredString = "";
+
+        if (isset(static::$foregroundColors[$foregroundColor])) {
+            $coloredString .= "\033[" . static::$foregroundColors[$foregroundColor] . "m";
+        }
+        if (isset(static::$backgroundColors[$backgroundColor])) {
+            $coloredString .= "\033[" . static::$backgroundColors[$backgroundColor] . "m";
+        }
+
+        $coloredString .= $string . "\033[0m";
+
+        return $coloredString;
+    }
+
+    /**
+     * 输出提示信息
+     * @param $msgß
+     */
+    public static function notice($msg)
+    {
+        fwrite(STDOUT, self::initColoredString($msg, 'light_gray') . PHP_EOL);
+    }
+
+    /**
+     * 输出错误信息
+     * @param $msg
+     */
+    public static function error($msg)
+    {
+        fwrite(STDERR, self::initColoredString($msg, 'red') . PHP_EOL);
+    }
+
+    /**
+     * 输出警告信息
+     * @param $msg
+     */
+    public static function warn($msg)
+    {
+        fwrite(STDOUT, self::initColoredString($msg, 'yellow') . PHP_EOL);
+    }
+
+    /**
+     * 输出成功信息
+     * @param $msg
+     */
+    public static function success($msg)
+    {
+        fwrite(STDOUT, self::initColoredString($msg, 'green') . PHP_EOL);
     }
 }
 
